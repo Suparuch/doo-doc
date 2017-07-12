888d26f81ebecf21006bb31152a9d92afdab10f9
Chayanon Visessopakul <random_hard_master@hotmail.com>
2017-07-12 10:12:56 +0700

    Fix DateLib

--- www/Lib/Utility/DateLib.php
+++ www/Lib/Utility/DateLib.php
@@ -4,12 +4,18 @@ class DateLib {
 
     // Convert dd-mm-yy in BE format to yy-mm-dd in AD format.
     public function convertBEToAD($date) {
+        if (empty($date)) {
+            return '';
+        }
         $newDate = explode("-", $date);
         return (intval($newDate[2])-543)."-".$newDate[1]."-".$newDate[0];
     }
 
     // Convert yy-mm-dd in AD format to dd-mm-yy in BE format.
     public function convertADToBE($date) {
+        if (empty($date)) {
+            return '';
+        }
         $newDate = explode("-", $date);
         return ($newDate[2])."-".($newDate[1])."-".(intval($newDate[0])+543);
     }
